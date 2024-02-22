99d676d670679cc0d8d6e9d452cc0eb550ab6b1b
Dan Pock <self@danpock.me>
2024-02-22 02:57:24 -0500

    add more failing tests

--- main_test.go
+++ main_test.go
@@ -8,7 +8,24 @@ import (
 func TestSpaceIdGenerator(t *testing.T) {
 	spaceHumanoid, err := SpaceIdGenerator()
 	assert.NoError(t, err)
+
 	res, err := spaceHumanoid.Create(0)
 	assert.NoError(t, err)
 	assert.Exactly(t, "andromeda", res)
+
+	res, err = spaceHumanoid.Create(1)
+	assert.NoError(t, err)
+	assert.Exactly(t, "backward", res)
+
+	res, err = spaceHumanoid.Create(2)
+	assert.NoError(t, err)
+	assert.Exactly(t, "bode", res)
+
+	res, err = spaceHumanoid.Create(3)
+	assert.NoError(t, err)
+	assert.Exactly(t, "cigar", res)
+
+	res, err = spaceHumanoid.Create(23)
+	assert.NoError(t, err)
+	assert.Exactly(t, "eris-pinwheel", res)
 }
